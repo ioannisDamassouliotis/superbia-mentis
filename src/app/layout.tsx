6452389b2eb00c65a9774019aa1bbc4f65811fe3
Dokiwised <ioannisdamassouliotis@gmail.com>
2025-03-05 18:04:47 +0200

    Fix Geist font import syntax

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,8 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
-const geistSans = GeistSans({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = GeistMono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const geistSans = GeistSans;
+const geistMono = GeistMono;
 
 export const metadata: Metadata = {
   title: "Superbia Mentis",
